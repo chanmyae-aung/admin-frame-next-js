6192f814f577890700a5affc6a6d8a10aa22e3fe
Chan Myae <143679555+chanmyaeaung-anbuchi@users.noreply.github.com>
2024-07-10 15:49:32 +0630

    editable rows (add || remove) in tanstack table

---
+++ src/app/(shared)/snippets/tanstack/data.ts
@@ -0,0 +1,33 @@
+export type Student = {
+  studentId: number;
+  name: string;
+  dateOfBirth: string;
+  major: string;
+};
+
+export const data: Student[] = [
+  {
+    studentId: 1,
+    name: "Bahar Constantia",
+    dateOfBirth: "1984-01-04",
+    major: "Computer Science",
+  },
+  {
+    studentId: 2,
+    name: "Harold Nona",
+    dateOfBirth: "1961-05-10",
+    major: "Communications",
+  },
+  {
+    studentId: 3,
+    name: "Raginolf Arnulf",
+    dateOfBirth: "1991-10-12",
+    major: "Business",
+  },
+  {
+    studentId: 5,
+    name: "Marvyn Wendi",
+    dateOfBirth: "1978-09-24",
+    major: "Psychology",
+  },
+];
